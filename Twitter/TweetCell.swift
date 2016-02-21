d6c7398ee34b882eae983ba7e93e9c72b85b68e0
DrDunkan <ddd155@msstate.edu>
2016-02-20 23:35:50 -0600

    Got something close to it

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -19,12 +19,21 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var tweetLabel: UILabel!
     
+    @IBOutlet weak var Redbut: UIButton!
+    @IBOutlet weak var RTbut: UIButton!
+    var Faved = false
+    var Tweeted = false
+    
+ 
+    
     var tweet: Tweet! {
         didSet {
             
         profileView.setImageWithURL(NSURL(string: tweet.user!.profileUrlString!)!)
         usernameLabel.text = tweet.user!.name as? String
         nameLabel.text = "@\(tweet.user!.screenname!)"
+        tweetLabel.text = tweet.text as? String
+        timeLabel.text = tweet.timeSince
         
         
             
@@ -43,5 +52,14 @@ class TweetCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
         
         // Configure the view for the selected state
+    }
+    @IBAction func RTButPres(sender: AnyObject) {
+        tweet.retweetCount! + 1
+        RTbut.backgroundColor = UIColor.blueColor()
+        
+    }
+    @IBAction func FavButPres(sender: AnyObject) {
+        tweet.favoritesCount! + 1
+        Redbut.backgroundColor = UIColor.redColor()
     }
 }
